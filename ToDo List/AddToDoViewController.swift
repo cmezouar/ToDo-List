c8c624cee3cf71bbbd22984bf614131f1d6fbd4e
cmezouar <chloe.mezouar@devinci.fr>
2018-11-28 10:49:29 -0800

    Creating outlets from Add ToDo view to controller and adding add button action.

--- ToDo List/AddToDoViewController.swift
+++ ToDo List/AddToDoViewController.swift
@@ -10,21 +10,15 @@ import UIKit
 
 class AddToDoViewController: UIViewController {
 
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func addButtonClicked(_ sender: Any) {
     }
-    */
-
 }
